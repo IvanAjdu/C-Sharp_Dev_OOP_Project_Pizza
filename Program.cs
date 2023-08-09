3db780d87fc70f3413e673a81c57fbe14d40f035
IvanAjdu <ivan.ajdu@gmail.com>
2023-08-09 03:32:36 +0200

    Class Pizza Fonction Afficher listePizzas Boucle foreach

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
-﻿namespace ProjetPIZZA
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace ProjetPIZZA
 {
+    
+    class Pizza
+    {
+        string nom;
+        float prix;
+        bool vegetarienne = false;
+
+        public Pizza(string nom, float prix, bool vegetarienne)
+        {
+            this.nom = nom;
+            this.prix = prix;
+            this.vegetarienne = vegetarienne;
+        }
+
+        public void AfficherPizza()
+        {
+            string badgeVege = " (V)";
+            if (!vegetarienne) { badgeVege = ""; }
+            Console.WriteLine("Pizza : " + nom + badgeVege + " - " + prix + "€");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var listePizzas = new List<Pizza>();
+            listePizzas.Add(new Pizza("Margherita", 7, true));
+            listePizzas.Add(new Pizza("Reine", 8.5f, false));
+            listePizzas.Add(new Pizza("5 fromages", 10.5f, true));
+            listePizzas.Add(new Pizza("Tartiflette", 12, false));
+
+            foreach(var pizza in listePizzas)
+            {
+                pizza.AfficherPizza();
+            }
         }
     }
 }
