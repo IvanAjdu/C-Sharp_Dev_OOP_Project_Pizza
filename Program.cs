89bf5ec6259d07491179bc2455a4a68082ec5028
IvanAjdu <ivan.ajdu@gmail.com>
2023-08-10 01:42:18 +0200

    Afficher pizza moins/plus cher. Algo de recherche manuel.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -7,8 +8,8 @@ namespace ProjetPIZZA
     
     class Pizza
     {
-        string nom;
-        float prix;
+        public string nom { get; private set; }
+        public float prix { get; private set; }
         bool vegetarienne = false;
         List<string> ingredients;
 
@@ -26,13 +27,14 @@ namespace ProjetPIZZA
             if (!vegetarienne) { badgeVege = ""; }
             Console.WriteLine(FormatMots(nom) + badgeVege + " - " + prix + "€");
 
-            List<string> ingredientsForm = new List<string>(ingredients);
-            for (int i = 0; i < ingredientsForm.Count; i++) 
+            /*var ingredientsAfficher = new List<string>();
+            foreach (var ingredient in ingredients)
             {
-                ingredientsForm[i] = FormatMots(ingredients[i]);
-            }
+                ingredientsAfficher.Add(FormatMots(ingredient));
+            }*/
+            var ingredientsAfficher = ingredients.Select(x => FormatMots(x)).ToList();
 
-            Console.WriteLine(string.Join(", ", ingredientsForm));
+            Console.WriteLine(string.Join(", ", ingredientsAfficher));
             Console.WriteLine();
         }
 
@@ -53,18 +55,46 @@ namespace ProjetPIZZA
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            var listePizzas = new List<Pizza>();
-            listePizzas.Add(new Pizza("Margherita", 7, true, new List<string>() {"tomate", "mozzarella", "basilic"}));
-            listePizzas.Add(new Pizza("Reine", 8.5f, false, new List<string>() { "tomate", "mozzarella", "champignon", "jambon" }));
-            listePizzas.Add(new Pizza("Calzone", 9.5f, false, new List<string>() { "tomate", "mozzarella", "jambon", "basicil" }));
-            listePizzas.Add(new Pizza("4 fromages", 10.5f, true, new List<string>() { "tomate", "mozzarella", "raclette", "parmesan", "roquefort" }));
-            listePizzas.Add(new Pizza("TARTiflette", 12, false, new List<string>() { "crème", "raclette", "lardons", "patates" }));
-            listePizzas.Add(new Pizza("veggie", 11, true, new List<string>() { "tomate", "mozzarella", "basicil", "roquette", "capres" }));
+            var Pizzas = new List<Pizza>();
+            Pizzas.Add(new Pizza("Margherita", 7, true, new List<string>() { "tomate", "mozzarella", "basilic" }));
+            Pizzas.Add(new Pizza("reine", 10f, false, new List<string>() { "tomate", "mozzarella", "champignon", "jambon" }));
+            Pizzas.Add(new Pizza("Calzone", 9.5f, false, new List<string>() { "tomate", "mozzarella", "jambon", "basicil" }));
+            Pizzas.Add(new Pizza("4 fromages", 10.5f, true, new List<string>() { "tomate", "mozzarella", "raclette", "parmesan", "roquefort" }));
+            Pizzas.Add(new Pizza("Tartiflette", 12, false, new List<string>() { "crème", "raclette", "lardons", "patates" }));
+            Pizzas.Add(new Pizza("veggie", 11, true, new List<string>() { "tomate", "mozzarella", "basicil", "roquette", "capres" }));
 
-            foreach (var pizza in listePizzas)
+            //listePizzas = listePizzas.OrderBy(x => x.prix).ToList();
+            //listePizzas = listePizzas.OrderByDescending(x => x.prix).ToList();
+
+            foreach (var pizza in Pizzas)
             {
                 pizza.AfficherPizza();
             }
+
+            Pizza pizzaPrixMin = Pizzas[0];
+            Pizza pizzaPrixMax = Pizzas[0];
+
+            foreach (var pizza in Pizzas)
+            {
+                if (pizzaPrixMin.prix > pizza.prix)
+                {
+                    pizzaPrixMin = pizza;
+                }
+            }
+
+            foreach (var pizza in Pizzas)
+            {
+                if (pizzaPrixMax.prix < pizza.prix)
+                {
+                    pizzaPrixMax = pizza;
+                }
+            }
+
+            Console.WriteLine("La pizza la moins cher est :");
+            pizzaPrixMin.AfficherPizza();
+
+            Console.WriteLine("La pizza la plus cher est :");
+            pizzaPrixMax.AfficherPizza();
         }
     }
-}
+}   
